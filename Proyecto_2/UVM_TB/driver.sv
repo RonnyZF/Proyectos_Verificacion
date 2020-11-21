fc356093c59599fe7aa1f88852964c51d862e0dc
Jean Paul Jimenez <jpjimenez12@gmail.com>
2020-11-20 20:19:00 -0600

    Driver Updated

--- Proyecto_2/UVM_TB/driver.sv
+++ Proyecto_2/UVM_TB/driver.sv
@@ -1,7 +1,5 @@
 class fpu_item extends uvm_sequence_item;
 
-  //rand logic [7:0] data;
-  //rand bit   rd;
   rand logic[31:0] opa, opb;
   rand logic[2:0] fpu_op;
   rand logic[1:0] rmode;
@@ -69,8 +67,6 @@ class fpu_driver extends uvm_driver #(fpu_item);
       `uvm_info("DRV", $sformatf("Wait for item from sequencer"), UVM_LOW)
       seq_item_port.get_next_item(f_item);
       fork
-        //drive_fpu(f_item);
-        //read_fpu(f_item);
         operation(f_item);
         overflow(f_item);
         div_zero(f_item);
@@ -80,119 +76,37 @@ class fpu_driver extends uvm_driver #(fpu_item);
   endtask  
 
   task operation(fpu_item f_item);
-    //repeat(iteration)
     begin
       $display("TASK OPERATION\n");
-      //sti = new();
       @ (negedge intf.clk);
       intf.opa = f_item.opa;
       intf.opb = f_item.opb;
       intf.fpu_op = f_item.fpu_op;
       intf.rmode = f_item.rmode;
-      //if(sti.randomize()) // Generate stimulus
-        //$display("decimal opa = %d and opb = %d in the DUT\n", sti.opa, sti.opb);
-        //opa = {sti.sign1, sti.exp1, sti.mantissa1};
-        //opb = {sti.sign2, sti.exp2, sti.mantissa2};
-        //intf.opa = opa; // Drive to DUT
-        //intf.opb = opb; // Drive to DUT
-        //$display("IEEE754 opa = %h and opb = %h in the DUT\n", intf.opa, intf.opb);
-        //intf.fpu_op = sti.fpu_op;
-      	//intf.rmode = sti.rmode;
-      //sb.opa.push_front(opa);// Cal exp value and store in Scoreboard
-      //sb.opb.push_front(opb);// Cal exp value and store in Scoreboard
-      //sb.fpu_op.push_front(sti.fpu_op);// Cal exp value and store in Scoreboard
-      //sb.rmode.push_front(sti.rmode);// Cal exp value and store in Scoreboard
     end
      @ (negedge intf.clk);
   endtask
 
   task overflow(fpu_item f_item);
-    //repeat(iteration)
     begin
-      //sti = new();
       @ (negedge intf.clk);
       intf.opa = 32'h7F7FFFFF;
       intf.opb = f_item.opb;
       intf.fpu_op = 3'b010;
       intf.rmode = f_item.rmode;
-      //if(sti.randomize()) // Generate stimulus
-        //$display("decimal opa = %d and opb = %d in the DUT\n", sti.opa, sti.opb);
-        //opb = {sti.sign2, sti.exp2, sti.mantissa2};
-        //intf.opa = 32'h7F7FFFFF; // Drive to DUT
-        //intf.opb = opb; // Drive to DUT
-        //$display("IEEE754 opa = %h and opb = %h in the DUT\n", intf.opa, intf.opb);
-        //intf.fpu_op = 3'b010;
-        //intf.rmode = sti.rmode;
-        //$display("FPU_OP = %d and RMODE = %d \n", sti.fpu_op, sti.rmode);
-      //sb.opa.push_front(intf.opa);// Cal exp value and store in Scoreboard
-      //sb.opb.push_front(intf.opb);// Cal exp value and store in Scoreboard
-      //sb.fpu_op.push_front(sti.fpu_op);// Cal exp value and store in Scoreboard
-      //sb.rmode.push_front(sti.rmode);// Cal exp value and store in Scoreboard
     end
      @ (negedge intf.clk);
   endtask
 
   task div_zero(fpu_item f_item);
-    //repeat(iteration)
     begin
-      //sti = new();
       @ (negedge intf.clk);
       intf.opa = f_item.opa;
       intf.opb = 32'h00000000;
       intf.fpu_op = 3'b011;
       intf.rmode = f_item.rmode;
-      //if(sti.randomize()) // Generate stimulus
-        //opa = {sti.sign1, sti.exp1, sti.mantissa1};
-        //intf.opa = opa; // Drive to DUT
-        //intf.opb = 32'h00000000;
-        //intf.fpu_op = 3'b011;
-        //intf.rmode = sti.rmode;
-      //sb.opa.push_front(intf.opa);// Cal exp value and store in Scoreboard
-      //sb.opb.push_front(intf.opb);// Cal exp value and store in Scoreboard
-      //sb.fpu_op.push_front(sti.fpu_op);// Cal exp value and store in Scoreboard
-      //sb.rmode.push_front(sti.rmode);// Cal exp value and store in Scoreboard
     end
      @ (negedge intf.clk);
-  endtask
-
-/*
-  virtual task drive_fpu(fpu_item f_item);
-       
-     @ (negedge intf.clk);
-     $display("Driving 0x%h value in the DUT\n", f_item.data);
-     intf.data_in = f_item.data; // Drive to DUT
-     intf.wr_en = 1;
-     @ (negedge intf.clk);
-     intf.wr_en = 0;
-  endtask
-  
-  virtual task read_fpu(fpu_item f_item);
-    if(f_item.rd ==1)
-      begin
-        @ (negedge intf.clk);
-        intf.rd_en = 1;
-        @ (negedge intf.clk);
-        intf.rd_en = 0;
-      end
-  endtask
-       
-  virtual task fpu_reset();  // Reset method
-    $display("Executing Reset\n");
-    intf.data_in = 0;
-    intf.rst = 0;
-    intf.wr_cs = 0;
-    intf.rd_cs = 0;
-    intf.data_in = 0;
-    intf.rd_en = 0;
-    intf.wr_en = 0;
-    intf.rst = 1;
-    @ (negedge intf.clk);
-    intf.rst = 0;
-    @ (negedge intf.clk);
-    intf.wr_cs = 1;
-    intf.rd_cs = 1;
-
-  endtask
-*/      
+  endtask    
   
 endclass
