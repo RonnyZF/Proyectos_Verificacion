3bcfefa5fd516f11895a95b0808f33fdc7c9b473
danileon21 <danileon21@gmail.com>
2020-11-20 20:01:24 -0600

    Add files via upload

--- Proyecto_2/UVM_TB/agent.sv
+++ Proyecto_2/UVM_TB/agent.sv
@@ -1,64 +1,66 @@
-class fifo_agent_active extends uvm_agent;
-  `uvm_component_utils(fifo_agent_active)
-  function new(string name="fifo_agent_active", uvm_component parent=null);
+//----------------Inicio de Agente Activo--------------------------//
+class fpu_agent_active extends uvm_agent;
+  `uvm_component_utils(fpu_agent_active)
+  function new(string name="fpu_agent_active", uvm_component parent=null);
     super.new(name, parent);
   endfunction
   
-  virtual fifo_intf intf;
-  fifo_driver fifo_drv;
-  uvm_sequencer #(fifo_item)	fifo_seqr;
+  //instacias
+  virtual fpu_intf intf;
+  fpu_driver fpu_drv;
+  uvm_sequencer #(fpu_item)	fpu_seqr;
+  fpu_monitor_wr fpu_mntr_wr;
 
-  fifo_monitor_wr fifo_mntr_wr;
-
+  //Creacion de los 3 objetos activos
   virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
+  super.build_phase(phase);
     
-    if(uvm_config_db #(virtual fifo_intf)::get(this, "", "VIRTUAL_INTERFACE", intf) == 0) begin
-      `uvm_fatal("INTERFACE_CONNECT", "Could not get from the database the virtual interface for the TB")
-    end
+  if(uvm_config_db #(virtual fpu_intf)::get(this, "", "VIRTUAL_INTERFACE", intf) == 0) 
+  begin
+    `uvm_fatal("INTERFACE_CONNECT", "Could not get from the database the virtual interface for the TB")
+  end
     
-    fifo_drv = fifo_driver::type_id::create ("fifo_drv", this); 
+  fpu_drv = fpu_driver::type_id::create ("fpu_drv", this); 
     
-    fifo_seqr = uvm_sequencer#(fifo_item)::type_id::create("fifo_seqr", this);
+  fpu_seqr = uvm_sequencer#(fpu_item)::type_id::create("fpu_seqr", this);
     
-    fifo_mntr_wr = fifo_monitor_wr::type_id::create ("fifo_mntr_wr", this);
+  fpu_mntr_wr = fpu_monitor_wr::type_id::create ("fpu_mntr_wr", this);
     
-    //uvm_config_db #(virtual fifo_intf)::set (null, "uvm_test_top.env.fifo_ag.fifo_drv", "VIRTUAL_INTERFACE", intf);    
-
   endfunction
 
+  //Conexion de sequencer con driver 
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    fifo_drv.seq_item_port.connect(fifo_seqr.seq_item_export);
+    fpu_drv.seq_item_port.connect(fpu_seqr.seq_item_export);
   endfunction
-
 endclass
-
-class fifo_agent_passive extends uvm_agent;
-  `uvm_component_utils(fifo_agent_passive)
-  function new(string name="fifo_agent_passive", uvm_component parent=null);
+//-------------------FIN de Agente Activo--------------------------//
+//-----------------------------------------------------------------//
+//----------------Inicio de Agente Pasivo--------------------------//
+class fpu_agent_passive extends uvm_agent;
+  `uvm_component_utils(fpu_agent_passive)
+  function new(string name="fpu_agent_passive", uvm_component parent=null);
     super.new(name, parent);
   endfunction
   
-  virtual fifo_intf intf;
-  
-  fifo_monitor_rd fifo_mntr_rd;
-  
+  //instancias
+  virtual fpu_intf intf;
+  fpu_monitor_rd fpu_mntr_rd;
+
+  //Creacion del objeto pasivo
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     
-    if(uvm_config_db #(virtual fifo_intf)::get(this, "", "VIRTUAL_INTERFACE", intf) == 0) begin
+  if(uvm_config_db #(virtual fpu_intf)::get(this, "", "VIRTUAL_INTERFACE", intf) == 0) 
+    begin
       `uvm_fatal("INTERFACE_CONNECT", "Could not get from the database the virtual interface for the TB")
     end
-    
-    fifo_mntr_rd = fifo_monitor_rd::type_id::create ("fifo_mntr_rd", this);
-
-    //uvm_config_db #(virtual fifo_intf)::set (null, "uvm_test_top.env.fifo_ag.fifo_drv", "VIRTUAL_INTERFACE", intf);    
-
+  fpu_mntr_rd = fpu_monitor_rd::type_id::create ("fpu_mntr_rd", this);
   endfunction
 
+  //No hay conexion
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
   endfunction
-
+//-------------------FIN de Agente Pasivo --------------------------//
 endclass
