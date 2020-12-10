29626c1ff56251ec8bbb38ddab8b57749d0ba6d3
Ronny Zárate <31499007+RonnyZF@users.noreply.github.com>
2020-12-09 23:32:17 -0600

    Add files via upload

---
+++ Proyecto_2/exe_sim.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir test1
+cd test1
+vcs -R -sverilog -F ../filelist -debug_access+all -full64 -cm line+tgl+assert +plusarg_save -timescale=1ns/1ns +UVM_TESTNAME=test_basic +ntb_random_seed=5
+cd ..
+
+mkdir test2
+cd test2
+vcs -R -sverilog -F ../filelist -debug_access+all -full64 -cm line+tgl+assert +plusarg_save -timescale=1ns/1ns +UVM_TESTNAME=test_basic +ntb_random_seed=45
+cd ..
+
+mkdir test3
+cd test3
+vcs -R -sverilog -F ../filelist -debug_access+all -full64 -cm line+tgl+assert +plusarg_save -timescale=1ns/1ns +UVM_TESTNAME=test_basic +ntb_random_seed=53
+cd ..
+
+mkdir test4
+cd test4
+vcs -R -sverilog -F ../filelist -debug_access+all -full64 -cm line+tgl+assert +plusarg_save -timescale=1ns/1ns +UVM_TESTNAME=test_basic +ntb_random_seed=12
+cd ..
+
+mkdir test5
+cd test5
+vcs -R -sverilog -F ../filelist -debug_access+all -full64 -cm line+tgl+assert +plusarg_save -timescale=1ns/1ns +UVM_TESTNAME=test_basic +ntb_random_seed=36
+cd ..
+
+mkdir test6
+cd test6
+vcs -R -sverilog -F ../filelist -debug_access+all -full64 -cm line+tgl+assert +plusarg_save -timescale=1ns/1ns +UVM_TESTNAME=test_basic2 +ntb_random_seed=516
+cd ..
+
+
