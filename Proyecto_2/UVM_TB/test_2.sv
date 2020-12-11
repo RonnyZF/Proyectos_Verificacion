334af3b619da00109fe5cc06341862a1e0255fab
RonnyZF <ronnyzarate@gmail.com>
2020-12-11 16:57:37 -0600

    m

--- Proyecto_2/UVM_TB/test_2.sv
+++ Proyecto_2/UVM_TB/test_2.sv
@@ -28,7 +28,7 @@ class gen_item_seq2 extends gen_item_seq;
     super.new(name);
   endfunction
   
-  int num = 300; 	// Config total number of items to be sent
+  int num = 300000; 	// Config total number of items to be sent
 
   //constraint c1 { num inside {[20:50]}; }
   
@@ -40,80 +40,80 @@ class gen_item_seq2 extends gen_item_seq;
 
     // Asercion 1
       `uvm_do(f_item,,,{f_item.opb==0;f_item.fpu_op==3;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 2
       `uvm_do(f_item,,,{f_item.opa==32'h3FA00000;f_item.opb==32'hBFA00000;f_item.fpu_op==0;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 3
-      $display("Aqui inicia asercion 3");
+      //$display("Aqui inicia asercion 3");
       `uvm_do(f_item,,,{f_item.opa==32'h3FA00000;f_item.opb==32'h3FA00000;f_item.fpu_op==1;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})  
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************  
     // Asercion 4a
       `uvm_do(f_item,,,{f_item.opa==0;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})   
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************   
     // Asercion 4b
       `uvm_do(f_item,,,{f_item.opb==0;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})    
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************    
     // Asercion 5
       `uvm_do(f_item,,,{f_item.opa==0;f_item.opb==2;f_item.fpu_op==3;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 9+
         `uvm_do(f_item,,,{f_item.opa==32'h7F800000;f_item.fpu_op==0;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opa==32'hFF800000;f_item.fpu_op==0;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})    
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************    
     // Asercion 9-
         `uvm_do(f_item,,,{f_item.opa==32'h7F800000;f_item.fpu_op==1;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opa==32'hFF800000;f_item.fpu_op==1;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 10+
         `uvm_do(f_item,,,{f_item.opb==32'h7F800000;f_item.fpu_op==0;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opb==32'hFF800000;f_item.fpu_op==0;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 10-
         `uvm_do(f_item,,,{f_item.opb==32'h7F800000;f_item.fpu_op==1;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opb==32'hFF800000;f_item.fpu_op==1;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})      
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************      
     // Asercion 11
         `uvm_do(f_item,,,{f_item.opa==32'h7F800000;f_item.opb==32'h3F800000;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opa==32'hFF800000;f_item.opb==32'h3F800000;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 12
         `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h7F800000;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'hFF800000;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 13
         `uvm_do(f_item,,,{f_item.opa==32'h7F800000;f_item.opb==32'h3F800000;f_item.fpu_op==3;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opa==32'hFF800000;f_item.opb==32'h3F800000;f_item.fpu_op==3;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})    
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************    
     // Asercion 14
         `uvm_do(f_item,,,{f_item.opa==32'h7F800000;f_item.opb==0;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opa==32'hFF800000;f_item.opb==0;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 15
         `uvm_do(f_item,,,{f_item.opa==32'h7F800000;f_item.opb==32'h7F800000;f_item.fpu_op==0;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 16
         `uvm_do(f_item,,,{f_item.opa==32'h7F800000;f_item.opb==32'h7F800000;f_item.fpu_op==3;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
         `uvm_do(f_item,,,{f_item.opa==32'hFF800000;f_item.opb==32'h7F800000;f_item.fpu_op==3;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 17
       `uvm_do(f_item,,,{f_item.opa==0;f_item.opb==0;f_item.fpu_op==3;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 18
       `uvm_do(f_item,,,{f_item.opa==32'h7F7FFFFF;f_item.opb==32'h40000000;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 18
       `uvm_do(f_item,,,{f_item.opa==32'h40000000;f_item.opb==32'h7F7FFFFF;f_item.fpu_op==2;})
-      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;})
+      `uvm_do(f_item,,,{f_item.opa==32'h3F800000;f_item.opb==32'h3F800000;f_item.fpu_op==0;}) //**********************
     // Asercion 20
       `uvm_do(f_item,,,{f_item.opa==32'h00000001;f_item.opb==32'h461C0000;f_item.fpu_op==3;})
     end
